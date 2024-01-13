86814a64055a9c57be217954106d1b82722d6661
Priyanuj Gogoi <priyanuj289@gmail.com>
2024-01-13 12:03:36 +0530

    [package] doc: add library doc
    Signed-off-by: Priyanuj Gogoi <priyanuj289@gmail.com>

--- lib/flowery_tts.dart
+++ lib/flowery_tts.dart
@@ -1,3 +1,12 @@
+/// Wraps Flowery Text-to-Speech API.
+///
+/// - [Website](https://flowery.pw)
+/// - [API Docs](https://flowery.pw/docs)
+/// - [OpenAPI Specification][1]
+///
+/// [1]: https://editor.swagger.io/?url=https://api.flowery.pw/openapi.json
+library;
+
 export 'src/enums.dart';
 export 'src/exceptions.dart';
 export 'src/flowery.dart';
